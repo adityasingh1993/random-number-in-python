0828456b57f341d207d693cc6d2a8b946a4be33e
adityasingh1993 <aditya.kumar@aot.edu.in>
2016-04-29 15:26:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 #-*-coding:utf8;-*-
 #qpy:2
-#qpy:console
+#qpy:consol
 
-print "This is console module"
+import time
+def multDigit(num):
+    prod=1
+    while(not num//10==0):
+       # num=num/10
+        prod*=num%10
+        print(prod)
+        num=num//10
+    print(prod)
+def calculateSum(num):
+    sum1=0
+    while(not num//10==0):
+       # num=num/10
+        sum1+=num%10
+        num=num//10
+    print(sum1)
+    multDigit(sum1)
+
+
+
+def getRandom(x,y):
+    now=float(time.time())
+    randNum=int(now*10)
+    print(randNum)
+    calculateSum(randNum)
+    
+
+    
+var=getRandom(4,5)"
